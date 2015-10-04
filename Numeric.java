7573e507a746bb620efd72ccee3650365171a894
laichunpongben <laichunpongben@gmail.com>
2015-10-04 16:03:16 +0800

    Create Numeric.java

---
+++ Numeric.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Numeric {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(Factorial(n));
+    }
+    
+    public static BigInteger Factorial(int i)
+    {
+        return (i <= 1) ? BigInteger.valueOf(1) : Factorial(i - 1).multiply(BigInteger.valueOf(i));
+    }
+}
